3dad377b92606bfb332ffb3d13420d44d7728e42
Pedro Amaro Lacerda <56603297+amaropedro@users.noreply.github.com>
2024-09-20 15:20:17 -0300

    FIX: more to navbar

---
+++ Autoload/main.gd
@@ -0,0 +1,22 @@
+extends Control
+
+@onready var rendered_scene: CanvasLayer = $RenderedScene
+@onready var nav_bar: HBoxContainer = $BottomNavBar/NavBarContainer/NavBar
+@onready var home: NavBtn = $BottomNavBar/NavBarContainer/NavBar/Home
+
+func _ready() -> void:
+	home._on_pressed()
+
+func clear_selected_btn():
+	for child in nav_bar.get_children():
+		child.set_enabled(false)
+
+func render(scene: PackedScene, btn: NavBtn):
+	clear_selected_btn()
+	btn.set_enabled(true)
+	
+	if rendered_scene.get_child_count() > 0:
+		rendered_scene.get_child(0).queue_free()
+	
+	var instance = scene.instantiate()
+	rendered_scene.add_child(instance)
--- Scenes/BarButtons/base_bar_btn.gd
+++ Scenes/BarButtons/base_bar_btn.gd
@@ -1,12 +1,16 @@
 extends Button
 
+class_name NavBtn
+
 @export var icon_file: Texture
+@export var page: PackedScene = null
 
 @onready var current: ColorRect = $Current
 @onready var icon_svg: TextureRect = $IconSVG
 
 func _ready() -> void:
 	icon_svg.texture = icon_file
+	set_enabled(false)
 
 func set_enabled(value: bool):
 	current.visible = value
@@ -16,3 +20,8 @@ func set_enabled(value: bool):
 		return
 	
 	icon_svg.modulate = Color(Meta.COLORS["NonFocus"], 1)
+
+func _on_pressed() -> void:
+	if page != null:
+		Main.render(page, self)
+		set_enabled(true)

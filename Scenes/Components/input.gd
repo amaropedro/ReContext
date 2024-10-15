ba001cb643667574ddd2163d3235939c04d7c4da
Pedro Amaro Lacerda <56603297+amaropedro@users.noreply.github.com>
2024-10-15 16:34:46 -0300

    FEAT: logo placeholder & fix line edit

--- Scenes/Components/input.gd
+++ Scenes/Components/input.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 	placeholder_text = placeholder
 
 func _on_focus_entered() -> void:
-	placeholder_text = "|"
+	placeholder_text = ""
 
 func _on_focus_exited() -> void:
 	placeholder_text = placeholder

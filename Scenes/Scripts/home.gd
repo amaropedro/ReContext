a61c5845f002827be2961333fb736e97fde4420b
Pedro Amaro Lacerda <56603297+amaropedro@users.noreply.github.com>
2024-10-21 15:44:26 -0300

    FEAT: quit btn in home screen

--- Scenes/Scripts/home.gd
+++ Scenes/Scripts/home.gd
@@ -4,3 +4,6 @@ const ChangeKey = preload("res://Scenes/ChangeKey.tscn")
 
 func _on_button_pressed() -> void:
 	SceneManager.main.render(ChangeKey)
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()

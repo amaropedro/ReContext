822bdefb9511a5ac161152c700d6a9869400ad53
Pedro Amaro Lacerda <56603297+amaropedro@users.noreply.github.com>
2024-11-14 12:46:09 -0300

    FEAT: how to use video link

---
+++ Scenes/Components/redirect.gd
@@ -0,0 +1,15 @@
+extends Panel
+
+@export var icon_svg: Texture
+@export var card_name: String
+
+@onready var icon_btn: TextureRect = $HomeBtn/Container/Icon
+@onready var name_btn: RichTextLabel = $HomeBtn/Container/VBoxContainer/Name
+
+func _ready() -> void:
+	icon_btn.texture = icon_svg
+	name_btn.text = "[center][color=47BDA8][font_size=30]"
+	name_btn.text += card_name
+
+func _on_home_btn_pressed() -> void:
+	OS.shell_open("https://youtu.be/33aTxHSHxOg")
